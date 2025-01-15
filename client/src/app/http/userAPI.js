dd1681eecf3790e5f07d1798e3d3b852ccc8e902
Serega <aleksei87-2010@mail.ru>
2025-01-16 01:03:08 +0300

    testing fix AppRouter

--- client/src/app/http/userAPI.js
+++ client/src/app/http/userAPI.js
@@ -18,6 +18,13 @@ export const login = async (email, password) => {
 
 export const check = async () => {
     const { data } = await $authHost.post("api/users/auth");
+
+    if (!data.token) {
+        throw new Error("Токен отсутствует в ответе сервера.");
+    }
+
+    const decodedToken = jwtDecode(data.token);
     localStorage.setItem("token", data.token);
+
     return { user: decodedToken, token: data.token };
 };
