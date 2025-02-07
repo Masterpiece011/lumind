20e501bfc523b09b1ed3ccf4003a69d0f2881332
Yura Korchagin <yu.p.korchagin@labmedia.su>
2025-02-07 20:54:57 +0300

    доработки вебсокета

---
+++ server/ws/messages-types.js
@@ -0,0 +1,30 @@
+// ПОДКЛЮЧЕНИЕ
+
+export const CONNECTION = "connection";
+
+// ДАННЫЕ ПОЛЬЗОВАТЕЛЯ
+
+export const UPDATE_USER_DATA = "update_user_data";
+
+// УВЕДОМЛЕНИЯ
+
+export const UPDATE_NOTIFICATIONS = "update_notifications";
+
+// КОМАНДЫ
+
+export const UPDATE_TEAMS = "update_teams";
+
+// ЧАТЫ
+
+export const UPDATE_CHATS = "update_chats";
+export const UPDATE_CHAT_MESSAGES = "update_chat_messages";
+
+// ПУБЛИКАЦИИ
+
+export const UPDATE_PUBLICATIONS = "update_publications";
+export const UPDATE_PUBLICATIONS_COMMENTS = "update_publications_comments";
+
+// КОМАНДЫ
+
+export const UPDATE_ASSIGNMENTS = "update_assignments";
+export const UPDATE_ASSIGNMENT = "update_assignments";
