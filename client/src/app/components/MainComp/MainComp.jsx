fcadedf86808c4fd9a57b558617002cac743a49e
Serega <aleksei87-2010@mail.ru>
2025-02-11 18:56:07 +0300

    add logic use SPA + NextRouter for MainComp

--- client/src/app/components/MainComp/MainComp.jsx
+++ client/src/app/components/MainComp/MainComp.jsx
@@ -1,33 +1,39 @@
 "use client";
 
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import { HeaderComp } from "../HeaderComp";
 import { TeamsPage } from "../views/TeamsComp";
 import { AssignmentsPage } from "../views/Assignments";
-import { UsersPage } from "../views/UsersComp";
 import Icon from "@/app/ui/icons/Icon";
-
 import TeamDetailPage from "@/app/teams/[id]/page";
 import AssignmentsDetailPage from "@/app/assignments/[id]/page";
-
 import * as styles from "./MainComp.module.scss";
-
 import Home from "@/app/assets/icons/home-icon.svg";
 import Chat from "@/app/assets/icons/chat-icon.svg";
 import Teams from "@/app/assets/icons/teams-icon.svg";
 import Assignments from "@/app/assets/icons/assignments-icon.svg";
 import Notifications from "@/app/assets/icons/notification-icon.svg";
+import { UsersPage } from "../views/UsersComp";
 
 const MainComp = () => {
-    const [selectedComponent, setSelectedComponent] = useState("teams");
-    const [selectedTeamId, setSelectedTeamId] = useState(null);
-    const [selectedAssignmentId, setSelectedAssignmentId] = useState(null);
+    const router = useRouter();
+    const pathname = usePathname();
+    const params = useParams();
+
+    const handleSelectTeam = (teamId) => {
+        router.push(`/teams/${teamId}`);
+    };
+
+    const handleSelectAssignment = (assignmentId) => {
+        router.push(`/assignments/${assignmentId}`);
+    };
 
     return (
         <div className={styles.container}>
             <header>
-                <HeaderComp setSelectedComponent={setSelectedComponent} />
+                <HeaderComp />
             </header>
             <div className={styles.mainContent}>
                 <aside className={styles.sidebar}>
@@ -35,69 +41,45 @@ const MainComp = () => {
                         <Icon
                             src={Home}
                             alt="home"
-                            onClick={() => setSelectedComponent("teams")}
+                            onClick={() => router.push("/")}
                         />
-
                         <Icon
                             src={Notifications}
                             alt="notifications"
-                            onClick={() =>
-                                setSelectedComponent("notifications")
-                            }
+                            onClick={() => router.push("/notifications")}
                         />
-
                         <Icon
                             src={Teams}
                             alt="teams"
-                            onClick={() => setSelectedComponent("teams")}
+                            onClick={() => router.push("/teams")}
                         />
-
                         <Icon
                             src={Chat}
                             alt="chat"
-                            onClick={() => setSelectedComponent("chat")}
+                            onClick={() => router.push("/chat")}
                         />
-
                         <Icon
                             src={Assignments}
                             alt="assignments"
-                            onClick={() => setSelectedComponent("assignments")}
+                            onClick={() => router.push("/assignments")}
                         />
                     </ul>
                 </aside>
 
                 <section className={styles.pageContent}>
-                    {selectedComponent === "teams" && (
-                        <TeamsPage
-                            onSelectTeam={(id) => {
-                                setSelectedTeamId(id);
-                                setSelectedComponent("teamDetail");
-                            }}
-                        />
-                    )}
-                    {selectedComponent === "teamDetail" && (
-                        <TeamDetailPage id={selectedTeamId} />
-                    )}
-
-                    {selectedComponent === "assignments" && (
+                    {pathname.startsWith("/teams/") && params.id ? (
+                        <TeamDetailPage id={params.id} />
+                    ) : pathname.startsWith("/assignments/") && params.id ? (
+                        <AssignmentsDetailPage id={params.id} />
+                    ) : pathname.startsWith("/teams") ? (
+                        <TeamsPage onSelectTeam={handleSelectTeam} />
+                    ) : pathname.startsWith("/assignments") ? (
                         <AssignmentsPage
-                            onSelectAssignment={(id) => {
-                                setSelectedAssignmentId(id);
-                                setSelectedComponent("assignmentDetail");
-                            }}
+                            onSelectAssignment={handleSelectAssignment}
                         />
+                    ) : (
+                        <h1>Добро пожаловать!</h1>
                     )}
-                    {selectedComponent === "assignmentDetail" && (
-                        <AssignmentsDetailPage id={selectedAssignmentId} />
-                    )}
-
-                    {selectedComponent === "notifications" && (
-                        <div>Страница уведомлений</div>
-                    )}
-
-                    {selectedComponent === "chats" && <div>Страница чата</div>}
-
-                    {selectedComponent === "users" && <UsersPage />}
                 </section>
             </div>
         </div>
